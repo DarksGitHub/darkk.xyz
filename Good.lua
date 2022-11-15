b12bf0e4fe390d6d4513c8162b2cf092413f7e3b
Darkk <108237499+DarksGitHub@users.noreply.github.com>
2022-11-14 19:56:00 -0500

    Delete Good.lua

--- Good.lua
+++
@@ -1,9 +0,0 @@
-shared.Settings - {
-    Autoplay = false
-    ReleaseDelay = 2
-        
-    Presentages - {
-        ["Good"] = s2
-    }
-]
-loadstring(game:HttpGet("https://github.com/Meifryk/octo/blob/main/semi%20rage"))()
