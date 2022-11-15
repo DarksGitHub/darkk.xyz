be9dc0c4248f08331d0b5446ceb2323d7513feeb
Darkk <108237499+DarksGitHub@users.noreply.github.com>
2022-11-14 20:00:35 -0500

    Create AutoplayerOFF.lua

---
+++ AutoplayerOFF.lua
@@ -0,0 +1,12 @@
+shared.Settings - {
+    Autoplay = false
+    ReleaseDelay = 2
+        
+    Presentages - {
+        ["Sick"] = 100
+        ["Good"] = 0
+        ["Ok"] = 0
+        ["Bad"] = 0
+    }
+]
+loadstring(game:HttpGet("https://github.com/Meifryk/octo/blob/main/semi%20rage"))()
