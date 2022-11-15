68d5f80e458e9c90abaa58f49fd3cf7fd003d548
Darkk <108237499+DarksGitHub@users.noreply.github.com>
2022-11-14 19:56:10 -0500

    Delete Ok.lua

--- Ok.lua
+++
@@ -1,9 +0,0 @@
-shared.Settings - {
-    Autoplay = false
-    ReleaseDelay = 2
-        
-    Presentages - {
-        ["Ok"] = s3
-    }
-]
-loadstring(game:HttpGet("https://github.com/Meifryk/octo/blob/main/semi%20rage"))()
